c5bc580e09e9a0ef96dab5e825e2457dffdbe686
JAIMEjun10r <jaimegenerali@gmail.com>
2023-12-08 10:00:40 -0300

    ConfirmBox

--- cypress/e2e/popup_alerts.cy.js
+++ cypress/e2e/popup_alerts.cy.js
@@ -27,7 +27,7 @@ describe('Lidando com Popups e Alerts', () => {
       .click()
   })
 
-  // Gleb has refactore this code. 
+  // Gleb has refactored this code. 
   it('Outra maneira para o Modal Popup', () => {
     cy.intercept('/**').as('googleAnalytics');
     cy.get('p > a')
@@ -37,7 +37,7 @@ describe('Lidando com Popups e Alerts', () => {
     cy.wait('@googleAnalytics')
     cy.get('#button1 > p')
       .click()
-    //I have created this function just for fun (it was to show to a friendo of mine)
+    //I have created this function just for fun (it was to show it to a friend of mine)
     cy.get('.modal-body').invoke('text').then((text) => {
       const txtEsperado = 'The waiting game can be a tricky one; this exercise will hopefully improve your understandings of the various types of waits.'
       const txtNaPagina = text.trim()
@@ -49,24 +49,28 @@ describe('Lidando com Popups e Alerts', () => {
 
   });
 
-  it('Javascript ConfirmBox - Pressing Cancel - I have never done it like this before', () => {
+
+  // I have never done it like this before
+  it.only('Javascript ConfirmBox - Pressing Cancel', () => {
     cy.window().then((win) => {
       cy.stub(win, 'confirm').returns(false);
     });
 
     // Clica no botão que aciona a Confirm Box
     cy.get('#button4').click();
-    const algo = 'Press a button!'
+    const msg = 'Press a button!'
 
     // Espera pela execução do stub
-    cy.window().its('confirm').should('have.been.calledOnceWith', algo);
+    cy.window().its('confirm').should('have.been.calledOnceWith', msg);
 
-    // Asserts relacionados à mensagem após pressionar Cancelar
+    // Asserts relacionadas à mensagem após pressionar Cancelar
       const txtCancelar = 'You pressed Cancel!';
       cy.get('#confirm-alert-text').should('be.visible').and('contain', txtCancelar)
   })
 
-  it.only('Javascript ConfirmBox - Pressing Cancel - I have always done it this way', () => {
+
+  // I have always done it this way
+  it.only('Javascript ConfirmBox - Pressing Cancel - That is the way I have been doing', () => {
     cy.get('#button4').click()
     cy.on('window:confirm', () => false)
     
@@ -74,6 +78,7 @@ describe('Lidando com Popups e Alerts', () => {
     cy.contains('#confirm-alert-text', txtConfirm).should('be.visible')
   })
 
+  // I have always done it this way
   it.only('Javascript ConfirmBox - Accepting', () => {
     cy.get('#button4').click()
     cy.on('window:confirm', (text) => {
